8ace644d075b5591c2704ac45a5801c90b833ffc
Aishwarya Vhatkar <avhatkar@global.sial.com>
2019-12-27 16:19:39 +0530

    app base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import HelloWorld from './HelloWorld';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        {`This is App.js`}
+        <HelloWorld/>
+      </div>
+    )
+  }
+};
---
+++ src/HelloWorld/index.js
@@ -0,0 +1,12 @@
+// HelloWorld.js
+import React,{Component} from 'react';
+
+export default class HelloWorld extends Component{
+    render(){
+        return(
+            <h1>
+                {`Hello World. I am Aishwarya`}
+            </h1>
+        )
+    }
+}
